167d1402c8b348ced3e7e0779edc654c444a686d
Tanay <Tanay@Tanays-MacBook-Pro.local>
2018-05-13 23:27:43 -0700

    json reading of mongo credentials

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+//-----------------MONGO CONN.----------------------
 // var http = require('http');
 // var url = require('url');
 
 
 // var MongoClient = require('mongodb').MongoClient;
 
-// var uri = "mongodb://tponkshe:DateNight123*@datenightcluster-shard-00-00-vxfwy.mongodb.net:27017,datenightcluster-shard-00-01-vxfwy.mongodb.net:27017,datenightcluster-shard-00-02-vxfwy.mongodb.net:27017/admin?ssl=true&replicaSet=DateNightCluster-shard-0&authSource=admin";
-// MongoClient.connect(uri, function(err, db) {
-//     if(err) throw err;
-//     console.log(db.s)
-//     db.close();
-// });
 
+// var fs = require("fs");
+//  var mongoData = fs.readFileSync("./credentials/mongoCredentials.json");
+//  var jsonContent = JSON.parse(mongoData);
+//  var usr = jsonContent.mongo.user;
+//  var pass = jsonContent.mongo.pass;
+//  var uri = "mongodb://"+usr+":"+pass+"@datenightcluster-shard-00-00-vxfwy.mongodb.net:27017,datenightcluster-shard-00-01-vxfwy.mongodb.net:27017,datenightcluster-shard-00-02-vxfwy.mongodb.net:27017/admin?ssl=true&replicaSet=DateNightCluster-shard-0&authSource=admin";
+// MongoClient.connect(uri, function(err, db) {
+//       if(err) throw err;
+//       console.log(db.s)
+//       db.close();
+// });
+//-----------------MONGO CONN.----------------------
 
 var express = require('express');
 ipfilter = require('express-ipfilter').IpFilter;
